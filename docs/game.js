21af1d17c062ee20e563624b3a83f6b76a78133d
djEjs <52478827+djEjs@users.noreply.github.com>
2026-08-23 15:21:07 +0900

    빌드

--- docs/game.js
+++ docs/game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"066e93d4-b627-42a2-94b8-3eea549505e3","remote_package_size":12789076,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":12789076,"audio":false}]});
+  loadPackage({"package_uuid":"b835fce5-fd9d-4b4a-af63-8ce27554e3c3","remote_package_size":12799416,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":12799416,"audio":false}]});
 
 })();
